f9c7858459b389e48c82b3e434d1428d67eaa804
Kathy Nguyen <kathyn262@gmail.com>
2019-10-12 21:15:02 -0700

    cleaned up code formatting

--- src/RegisterForm.js
+++ src/RegisterForm.js
@@ -25,38 +25,51 @@ class RegisterForm extends React.Component {
 
   async handleSubmit(evt) {
     evt.preventDefault();
-    let token = await JobBoxApi.register(this.state.username, this.state.password, this.state.firstName, this.state.lastName, this.state.email);
+    let token = await JobBoxApi.register(this.state.username, this.state.password,
+      this.state.firstName, this.state.lastName, this.state.email);
     localStorage.setItem("token", token.token);
     this.props.history.push('/')
   }
 
   render() {
     return (
-      <div>
-        <AuthNav signUpActive={true} />
-        <form className="register-form" onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input type="text" className="form-control" id="username" aria-describedby="username" placeholder="Username" name="username" value={this.state.username} onChange={this.handleChange} />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input type="password" className="form-control" id="password" placeholder="Password" name="password" value={this.state.password} onChange={this.handleChange} />
-          </div>
-          <div className="form-group">
-            <label htmlFor="firstName">First Name</label>
-            <input type="text" className="form-control" id="firstName" aria-describedby="first name" placeholder="First Name" name="firstName" value={this.state.firstName} onChange={this.handleChange} />
-          </div>
-          <div className="form-group">
-            <label htmlFor="lastName">Last Name</label>
-            <input type="text" className="form-control" id="lastName" aria-describedby="last name" placeholder="Last Name" name="lastName" value={this.state.lastName} onChange={this.handleChange} />
-          </div>
-          <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <input type="text" className="form-control" id="email" aria-describedby="email" placeholder="Email" name="email" value={this.state.email} onChange={this.handleChange} />
-          </div>
-          <button type="submit" className="btn register">Submit</button>
-        </form>
+      <div className="row justify-content-center align-items-center register-row">
+        <div className="col-6">
+          <AuthNav signUpActive={true} />
+          <form className="register-form" onSubmit={this.handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="username">Username</label>
+              <input type="text" className="form-control" id="username"
+                aria-describedby="username" placeholder="Username" name="username"
+                value={this.state.username} onChange={this.handleChange} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" className="form-control" id="password"
+                placeholder="Password" name="password" value={this.state.password}
+                onChange={this.handleChange} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="firstName">First Name</label>
+              <input type="text" className="form-control" id="firstName"
+                aria-describedby="first name" placeholder="First Name" name="firstName"
+                value={this.state.firstName} onChange={this.handleChange} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="lastName">Last Name</label>
+              <input type="text" className="form-control" id="lastName"
+                aria-describedby="last name" placeholder="Last Name" name="lastName"
+                value={this.state.lastName} onChange={this.handleChange} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input type="text" className="form-control" id="email"
+                aria-describedby="email" placeholder="Email" name="email"
+                value={this.state.email} onChange={this.handleChange} />
+            </div>
+            <button type="submit" className="btn register">Submit</button>
+          </form>
+        </div>
       </div>
     );
   }
