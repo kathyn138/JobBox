75e2a5ab8644f55a4541c17d7c54c8f05fcb8ccf
Kathy Nguyen <kathyn262@gmail.com>
2019-10-14 20:21:16 -0700

    styled companydetails page

--- frontend/src/CompanyDetails.js
+++ frontend/src/CompanyDetails.js
@@ -13,18 +13,21 @@ class CompanyDetails extends React.Component {
 
   async componentDidMount() {
     let result = await JobBoxApi.getCompany(this.props.match.params.company);
-    this.setState({company: result})
+    this.setState({ company: result })
   }
 
   render() {
-    let jobs = this.state.company.jobs ? 
-      this.state.company.jobs.map(job => <JobCard applyToJob={this.props.applyToJob} checkApplied={this.props.checkApplied} job={job} key={job.id} />) : 
+    let jobs = this.state.company.jobs ?
+      this.state.company.jobs.map(job => <JobCard applyToJob={this.props.applyToJob}
+        checkApplied={this.props.checkApplied} job={job} key={job.id} />) :
       "Loading...";
     return (
-      <div>
-        <h1>{this.state.company.name}</h1>
-        <p>{this.state.company.description}</p>
-        {jobs}
+      <div className="row justify-content-center align-items-center">
+        <div className="col-8">
+          <h1 className="text-center">{this.state.company.name}</h1>
+          <p>{this.state.company.description}</p>
+          {jobs}
+        </div>
       </div>
     );
   }
