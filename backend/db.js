2285f7bee782ed68f61a59d5f7b6a12420377729
Kathy Nguyen <kathyn262@gmail.com>
2022-12-15 15:30:12 -0800

    removed unused config

--- backend/db.js
+++ backend/db.js
@@ -1,20 +1,17 @@
 /** Database setup for jobly. */
 
-
 const { Client } = require("pg");
-const { DB_URI } = require("./config");
 
 let config;
 
 if (process.env.DATABASE_URL) {
   config = {
-    connectionString: DB_URI,
-    ssl: {
-      rejectUnauthorized: false
-    }
+    connectionString: process.env.DATABASE_URL,
   };
 } else {
-  config = { connectionString: DB_URI }
+  config = {
+    connectionString: "postgresql:///jobbox",
+  };
 }
 
 const client = new Client(config);
