22986a28d847f3fe7d133cd7ef8f57956fcba676
Kathy Nguyen <kathyn262@gmail.com>
2023-04-03 17:54:34 -0700

    removed comment

--- frontend/src/PrivateRoute.tsx
+++ frontend/src/PrivateRoute.tsx
@@ -1,7 +1,6 @@
 import { Redirect, Route } from 'react-router-dom';
 import React from 'react';
 
-/// is any allowed in this case? 
 const PrivateRoute = ({ component: Component, ...rest }: any) => (
   <Route {...rest} render={(rtProps) => (
     rest.user
