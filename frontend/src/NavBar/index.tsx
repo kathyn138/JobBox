fa1c07392738ebab2c2ce7d0f15fc51b7401eb1d
Kathy Nguyen <kathyn262@gmail.com>
2023-04-03 17:37:39 -0700

    improved code reusability

--- frontend/src/NavBar/index.tsx
+++ frontend/src/NavBar/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink, RouteComponentProps } from "react-router-dom";
+import { NavLink, RouteComponentProps } from 'react-router-dom';
 import './NavBar.css';
 
 interface NavBarProps extends RouteComponentProps {
@@ -19,32 +19,62 @@ class NavBar extends React.PureComponent<NavBarProps> {
 
   render() {
     let loggedIn = localStorage.token ? true : false;
+
+    const navItems = [
+      { path: 'companies', name: 'Companies' },
+      { path: 'jobs', name: 'Jobs' },
+      { path: 'profile', name: 'Profile' },
+      { path: 'login', name: 'Logout', fn: this.logOut },
+    ];
+
+    // has links for: companies, jobs, profile, logout
+    const loggedInNav = navItems.map((i) => {
+      let fn;
+
+      if (i.path === 'login') {
+        fn = i.fn;
+      }
+
+      return (
+        <li className="nav-item">
+          <NavLink className="nav-link" to={`/${i.path}`} onClick={fn}>
+            {i.name}
+          </NavLink>
+        </li>
+      );
+    });
+
+    // only has link to log in
+    const loggedOutNav = (
+      <li className="nav-item">
+        <NavLink className="nav-link" id="nav-login" to="/login">
+          Login
+        </NavLink>
+      </li>
+    );
+
     return (
       <nav className="navbar navbar-expand-lg">
-        <NavLink className="navbar-brand" id="jb-nav" to="/">JobBox</NavLink>
-        <button className="navbar-toggler" type="button" data-toggle="collapse"
-          data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-          aria-label="Toggle navigation">
+        <NavLink className="navbar-brand" id="jb-nav" to="/">
+          JobBox
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-          {loggedIn ?
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/companies">Companies</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/jobs">Jobs</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/profile">Profile</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/login" 
-                  onClick={this.logOut}>Logout</NavLink>
-              </li>
-            </ul> : <ul className="navbar-nav"><li className="nav-item">
-              <NavLink className="nav-link" id="nav-login" to="/login">Login</NavLink></li></ul>}
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarNav"
+        >
+          <ul className="navbar-nav">
+            {loggedIn ? loggedInNav : loggedOutNav}
+          </ul>
         </div>
       </nav>
     );
