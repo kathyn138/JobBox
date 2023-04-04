00283f0ed73402692ef0f4e6700461797abf9338
Kathy Nguyen <kathyn262@gmail.com>
2023-04-03 17:54:58 -0700

    edited welcome msg

--- frontend/src/Home/index.tsx
+++ frontend/src/Home/index.tsx
@@ -20,9 +20,9 @@ class Home extends React.PureComponent<HomeProps> {
         <div className="col-6 text-center">
           {loggedIn ?
             <div className="home-logged-in">
-              <h3>Welcome Back, {this.props.user.user.firstName}!</h3>
-              <span className="logged-in-msg">Let's find you a job</span>
-              <Link className="btn btn-primary" id="home-jobs-button" to="/jobs">Job Postings</Link>
+              <h3>Welcome back, {this.props.user.user.firstName}!</h3>
+              <span className="logged-in-msg">Let's find you a job.</span>
+              <Link className="btn btn-primary" id="home-jobs-button" to="/jobs">Jobs</Link>
               <Link className="btn btn-primary" id="home-companies-button" to="/companies">Companies</Link>
               <img className="logged-in-home-img" src={loggedInHome} alt="collaborating on laptop"></img>
             </div>
