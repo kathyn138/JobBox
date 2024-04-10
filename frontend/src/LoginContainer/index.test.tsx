988e85dc01d1ce0ada9d252e0bf6b89d6d91d6e0
Kathy Nguyen <kathyn262@gmail.com>
2024-04-09 19:40:37 -0700

    mocked rcprops

--- frontend/src/LoginContainer/index.test.tsx
+++ frontend/src/LoginContainer/index.test.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 import { mount } from 'enzyme';
-import LoginContainer from './LoginContainer';
+import LoginContainer from './';
+
+const routeComponentPropsMock = {
+  history: {} as any,
+  location: {} as any,
+  match: {} as any,
+};
+
+let mockFn = jest.fn();
 
 it('can render the correct component', function () {
-  let wrapper = mount(<BrowserRouter><LoginContainer /></BrowserRouter>);
+  let wrapper = mount(
+    <BrowserRouter>
+      <LoginContainer {...routeComponentPropsMock} getCurrentUser={mockFn} />
+    </BrowserRouter>
+  );
 
   expect(wrapper.html()).toContain('Username');
 });
 
-it('can call mock function on form submission', function() {
-    let mockFn = jest.fn();
-    let wrapper = mount(<BrowserRouter><LoginContainer getCurrentUser={mockFn}/></BrowserRouter>);
-    wrapper.simulate('submit');
-    expect(mockFn).toHaveBeenCalled();
-})
+it('can call mock function on form submission', function () {
+  let wrapper = mount(
+    <BrowserRouter>
+      <LoginContainer {...routeComponentPropsMock} getCurrentUser={mockFn} />
+    </BrowserRouter>
+  );
+  wrapper.simulate('submit');
+  expect(mockFn).toHaveBeenCalled();
+});
